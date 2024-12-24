6c41f17bf1d37bf02aabd315efb0b82a4c631432
sagartiwari0608 <sagartiwarisrt@gmail.com>
2024-12-24 23:53:50 +0530

    updated the function call

--- alert&confirm.js
+++ alert&confirm.js
@@ -1,7 +1,9 @@
-
-function random(){
-console.log("alert will happen first ");
-alert("this is your alert click ok to continue");
-let confirmAns =confirm("are you sure that you want to continue? ... you can se your selected answer in console")
-console.log(confirmAns);
+function random() {
+  console.log("alert will happen first ");
+  alert("this is your alert click ok to continue");
+  let confirmAns = confirm(
+    "are you sure that you want to continue? ... you can se your selected answer in console"
+  );
+  console.log(confirmAns);
 }
+random();
